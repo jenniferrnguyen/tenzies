12b29caf7c968ff42d002c26f3c4239cb546a541
Jennifer Nguyen <jennifer.t.nguyen.02@gmail.com>
2025-10-05 22:48:09 -0700

    dice number random generat functionality"

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
 import Die from "./components/Die";
+import React from "react";
 
 export default function App() {
+  /**
+   * Challenge:
+   *
+   * Create state to hold our array of numbers. (Initialize
+   * the state by calling our `generateAllNewDice` function so it
+   * loads all new dice as soon as the app loads)
+   *
+   * Map over the state numbers array to generate our array
+   * of Die elements and render those in place of our
+   * manually-written 10 Die elements.
+   */
+  const [diceNumbers, setDiceNumbers] = React.useState(generateAllNewDice());
+
+  function generateAllNewDice() {
+    const newDice = [];
+
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+
+    return newDice;
+  }
+
+  const diceElements = diceNumbers.map((num, idx) => (
+    <Die key={idx} number={num} />
+  ));
+
   return (
     <main>
-      <div className="dice-container">
-        <Die number={1} />
-        <Die number={2} />
-        <Die number={3} />
-        <Die number={4} />
-        <Die number={5} />
-        <Die number={6} />
-        <Die number={1} />
-        <Die number={2} />
-        <Die number={3} />
-        <Die number={4} />
-      </div>
+      <div className="dice-container">{diceElements}</div>
     </main>
   );
 }
