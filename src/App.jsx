a261156d3414e7c4c9893486750f5d841f647fa5
Jennifer Nguyen <jennifer.t.nguyen.02@gmail.com>
2025-10-12 16:01:26 -0700

    add title and instructions

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
     for (let i = 0; i < 10; i++) {
       newDice.push({
         value: Math.ceil(Math.random() * 6),
-        isHeld: true,
+        isHeld: false,
         id: nanoid(),
       });
     }
@@ -41,6 +41,11 @@ export default function App() {
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="dice-container">{diceElements}</div>
       <button className="roll-dice-btn" onClick={rollDice}>
         Roll
